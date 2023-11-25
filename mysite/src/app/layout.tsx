59997bc2f7c3f232c924e439f425b338540ff855
Arsalan <arsalan.akkhter@gmail.com>
2023-11-25 21:23:04 +0500

    Navbar and CV download completed..

---
+++ mysite/src/app/layout.tsx
@@ -0,0 +1,38 @@
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import Navbar from './components/Navbar' // importing Navbar components from Components folder
+import './globals.css'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'M. ARSALAN AKHTAR',
+  description: 'Portfolio website',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      {
+        /*
+        */
+      }
+      <head> 
+
+      </head>
+      <body>
+      <Navbar/>        
+       
+        {
+        
+        children
+        
+        }
+      </body>
+    </html>
+  )
+}
